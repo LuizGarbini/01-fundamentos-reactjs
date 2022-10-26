ff368ec5f865f5c9cc2edc1d60939bf2f47ac3a4
LuizGarbini <garbasneto@gmail.com>
2022-10-26 13:28:02 -0300

    :construction: first module

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Post } from './Post';
+import { Post } from "./Post";
 
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Luiz Garbini Neto"
+        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi odio deserunt similique, odit repellendus omnis incidunt velit nobis blanditiis nulla dignissimos laboriosam rerum ullam illo ipsa soluta vitae explicabo eos."
+      />
+      <Post 
+      author="Gabriel Buzzi"
+      content="Um novo post muito legal"
+      />
     </div>
   );
 }
