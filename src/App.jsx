dc3cb50ea163903ccc93326659df7f6da118b56c
LuizGarbini <garbasneto@gmail.com>
2022-10-27 12:20:20 -0300

    :wrench:just updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+import { Header } from "./components/Header.jsx";
 import { Post } from "./Post";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Luiz Garbini Neto"
         content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi odio deserunt similique, odit repellendus omnis incidunt velit nobis blanditiis nulla dignissimos laboriosam rerum ullam illo ipsa soluta vitae explicabo eos."
       />
-      <Post 
-      author="Gabriel Buzzi"
-      content="Um novo post muito legal"
-      />
+      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
     </div>
   );
 }
