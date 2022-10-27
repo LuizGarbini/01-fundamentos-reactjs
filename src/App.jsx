3dbe31d5919306e03b0ebd1f3cbeb46f8ac7018a
LuizGarbini <garbasneto@gmail.com>
2022-10-27 13:35:21 -0300

    ::wrench: add Sidebar.jsx/css and Header.jsx/css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { Header } from "./components/Header.jsx";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar.jsx";
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Luiz Garbini Neto"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi odio deserunt similique, odit repellendus omnis incidunt velit nobis blanditiis nulla dignissimos laboriosam rerum ullam illo ipsa soluta vitae explicabo eos."
-      />
-      <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Luiz Garbini Neto"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi odio deserunt similique, odit repellendus omnis incidunt velit nobis blanditiis nulla dignissimos laboriosam rerum ullam illo ipsa soluta vitae explicabo eos."
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
     </div>
   );
 }
