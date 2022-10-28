ca7417978a3facb8a42893cc2ebb6a6325d7bdf9
LuizGarbini <garbasneto@gmail.com>
2022-10-28 13:41:36 -0300

    :wrench: add post style

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,25 +18,30 @@ export function Post() {
         <time title="27 de Outubro às 22:15" dateTime="2022-10-27 22:15:30">
           Publicado há 1h
         </time>
-
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-
-          <p>
-            👉 <a href="">jane.design/doctorcare</a>
-          </p>
-
-          <p>
-            <a href="">#novoprojeto #nlw #rocketseat</a>
-          </p>
-        </div>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit"> Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
