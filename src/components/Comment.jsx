265f061c35a40a1b6ebc7ad72edb4cd636731585
LuizGarbini <garbasneto@gmail.com>
2022-10-31 13:24:22 -0300

    :wrench: add comment.jsx/css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/luizgarbini.png"></img>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong> Luiz Garbini Neto</strong>
+              <time
+                title="27 de Outubro às 22:15"
+                dateTime="2022-10-27 22:15:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p> Muito bom Devon, parabéns!! 👏👏 </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
