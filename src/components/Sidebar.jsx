3d8357c5260afd33b3326d0cb42d2a0a735df47d
LuizGarbini <garbasneto@gmail.com>
2022-11-07 21:38:23 -0300

    :wrench: finish second module

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -11,10 +12,7 @@ export function Sidebar() {
       ></img>
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/LuizGarbini.png"
-        />
+        <Avatar src="https://github.com/luizgarbini.png" />
 
         <strong>Luiz Garbini Neto</strong>
         <span>Web Developer</span>
